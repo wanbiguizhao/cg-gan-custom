4c3834a88ee799d1e315ee8641e2429521aea65b
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-05-05 16:07:25 +0800

    支持转化多种字体

--- util/font2img.py
+++ util/font2img.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 import sys
 
 import argparse
@@ -11,6 +12,7 @@ import json
 import collections
 import re
 from fontTools.ttLib import TTFont
+#from fontTools.ttx import TTFont
 from tqdm import tqdm
 import random
 
@@ -20,30 +22,60 @@ PROJECT_DIR=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(PROJECT_DIR)
 from charset_util import processGlyphNames
 
-src_fonts_path="data/font/方正楷体_GBK.ttf"
-src_font = TTFont(src_fonts_path)
-src_char_set = processGlyphNames(src_font.getGlyphNames())
-print(len(src_char_set))
 
 
-def font2img(src_font,dst_img_dir):
+def is_contains_chinese(strs):
+    for _char in strs:
+        if '\u4e00' <= _char <= '\u9fa5':
+            return True
+    return False
+def font2img(base_src_font_dir,base_dst_img_dir):
+    def _font2img(src_font_dir,dst_img_dir):
+        font=TTFont(src_font_dir)
+        charset = processGlyphNames(font.getGlyphNames())
+        print(src_font_dir,len(charset))
+        img_font=ImageFont.truetype(src_font_dir,64)
+        for index, ch in enumerate(charset):
+            if not is_contains_chinese(ch):
+                continue
+            img = Image.new("L", (canvas_size*2 , canvas_size*2 ), 0)
+            draw = ImageDraw.Draw(img)
+            draw.text((0, 0), ch, 255, font=img_font)
+            bbox = img.getbbox()
+            l, u, r, d = bbox
+            l = max(0, l - 5)
+            u = max(0, u - 5)
+            r = min(canvas_size * 2 - 1, r + 5)
+            d = min(canvas_size * 2 - 1, d + 5)
+            text_region_img=img.crop((l,u,r,d))
+            text_high, text_wide = text_region_img.size
+            new_len_side=max(text_high,text_wide)
+            square_img=Image.new("L",(new_len_side,new_len_side),0)
+            square_img.paste(text_region_img,( (new_len_side-text_high)//2, (new_len_side-text_wide)//2))
+            square_img = square_img.resize((canvas_size, canvas_size), Image.Resampling.LANCZOS)
+
+            np_square_img=255-np.array(square_img)
+            square_img=Image.fromarray(np_square_img)
+            square_img.save(f"{dst_img_dir}/_{ch}.png")
+            if index>200:
+                break
+    
     canvas_size=64
-    assert src_font is not None
-    assert os.path.exists(src_font)
-    if not os.path.isdir(dst_img_dir):
-        os.makedirs(dst_img_dir)
-    font = TTFont(src_font)
-    charset = processGlyphNames(font.getGlyphNames())
-    img_font=ImageFont.truetype(src_font,64)
-    for ch in charset:
+    assert base_src_font_dir is not None
+    assert os.path.exists(base_src_font_dir)
+    font_instance_dict={}
+    shutil.rmtree(base_dst_img_dir)
+    for index,font_file in enumerate(os.listdir(base_src_font_dir)):
+        if not os.path.exists(f"{base_dst_img_dir}/{index}"):
+            os.makedirs(f"{base_dst_img_dir}/{index}")
+        print(index,font_file)
+        _font2img(f"{base_src_font_dir}/{font_file}",f"{base_dst_img_dir}/{index}")
         
-        img = Image.new("L", (canvas_size , canvas_size ), 0)
-        draw = ImageDraw.Draw(img)
-        draw.text((0, 0), ch, 255, font=img_font)
-        
-        print("hehe",ch)
-        bbox = img.getbbox()
-        print(bbox)
+    # 生成所有字体文件图片信息
+
+
+    
+
         # l, u, r, d = bbox
         # l = max(0, l - 5)
         # u = max(0, u - 5)
@@ -53,11 +85,7 @@ def font2img(src_font,dst_img_dir):
         # text_region_img=img.crop((l,u,r,d))
         # text_high, text_wide = text_region_img.size
         # new_len_side=max(text_high,text_wide)
-        np_square_img=255-np.array(img)
-        square_img=Image.fromarray(np_square_img)
-        square_img.save(f"{dst_img_dir}/{ch}.png")
-        break
-    
+
 
 
 def default_args_parser():
@@ -69,4 +97,5 @@ def default_args_parser():
 if __name__ == "__main__":
     parser=default_args_parser()
     args = parser.parse_args()
-    font2img(src_font=args.src_font,dst_img_dir=args.dst_img_dir)
+    #font2img(base_src_font_dir=args.src_font,base_dst_img_dir=args.dst_img_dir)
+    font2img("/home/liukun/share/font/","tmp/images")
